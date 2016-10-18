d4555c8d375180a343c2658dc4c0509a61a2eb17
tharvik <tharvik@users.noreply.github.com>
2016-10-18 16:51:32 +0200

    add ip

--- lab2/topo1.py
+++ lab2/topo1.py
@@ -18,6 +18,7 @@ def firstNetwork():
     h2 = net.addHost( 'h2', ip='10.0.0.2/24' )
     h3 = net.addHost( 'h3', ip='10.0.0.3/31' )
     h4 = net.addHost( 'h4', ip='10.0.1.4/24' )
+    PC = {1: h1, 2: h2, 3: h3, 4: h4}
 
     info( '*** Adding switches\n' )
     s12 = net.addSwitch( 's12' )
@@ -32,7 +33,6 @@ def firstNetwork():
 
     info( '*** Starting network\n')
     net.start()
-    "This is used to run commands on the hosts"
 
     info( '*** Starting xterm on hosts\n' )
     h1.cmd('xterm -xrm \'XTerm.vt100.allowTitleOps: false\' -T h1 &')
@@ -40,16 +40,32 @@ def firstNetwork():
     h3.cmd('xterm -xrm \'XTerm.vt100.allowTitleOps: false\' -T h3 &')
     h4.cmd('xterm -xrm \'XTerm.vt100.allowTitleOps: false\' -T h4 &')
 
-    info( '*** Running the command line interface\n' )
+    info('*** ovs-ofctl\n')
     os.system('ovs-ofctl add-flow s12 action=flood')
     os.system('ovs-ofctl add-flow s34 action=flood')
+
+    info('*** set IP\n')
+    for pc in PC.values():
+        for intf in pc.intfNames():
+            pc.cmd('ip addr flush dev {}'.format(intf))
+
+    for i in range(1, 4):
+        pc = PC[i]
+        pc_ip = '10.0.0.{}/24'.format(i)
+        pc.cmd('ip addr add {} dev {}-eth0'.format(pc_ip, pc.name))
+    PC[4].cmd('ip addr add 10.0.1.4/24 dev {}-eth0'.format(PC[4].name))
+
+    info('*** up interfaces\n')
+    for pc in PC.values():
+        for intf in pc.intfNames():
+            pc.cmd('ip link set {} up'.format(intf))
+
+    info( '*** Running the command line interface\n' )
     CLI( net )
 
     info( '*** Closing the terminals on the hosts\n' )
-    h1.cmd("killall xterm")
-    h2.cmd("killall xterm")
-    h3.cmd("killall xterm")
-    h4.cmd("killall xterm")
+    for h in PC:
+        h.cmd("killall xterm")
 
     info( '*** Stopping network' )
     net.stop()
